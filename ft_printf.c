99581b9cd00e68ee8124aa3d8552aaee21bf554d
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2024-05-10 19:59:01 +0100

    MORE CHAOS

--- ft_printf.c
+++ ft_printf.c
@@ -6,44 +6,47 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 20:18:17 by htrindad          #+#    #+#             */
-/*   Updated: 2024/05/09 19:51:58 by htrindad         ###   ########.fr       */
+/*   Updated: 2024/05/10 19:35:48 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_callarg(va_list args, char c)
+static void	ft_callarg(va_list args, char c, int *print_l)
 {
-	int	print_l;
-
 	if (c == 99)
-		print_l = ft_putchar_fd(va_arg(args, char), 1);
+		ft_putchar_fd(va_arg(args, int), 1, print_l);
 	if (c == 115)
-		print_l = ft_putstr_fd(va_arg(args, char *), 1);
+		ft_putstr_fd(va_arg(args, char *), 1, print_l);
 	if (c == 112)
-		print_l = ft_putptr_fd(va_arg(args, unsigned long long), 1);
+		ft_putptr_fd(va_arg(args, unsigned long long), 1, print_l);
 	if (c == 100 || c == 105)
-		print_l = ft_putnbr_fd(va_arg(args, int), 1);
+		ft_putnbr_fd(va_arg(args, int), 1, print_l);
 	if (c == 117)
-		print_l = ft_putuns_fd(va_arg(args, unsigned int), 1);
-	return (print_l);
+		ft_print_ung_fd(va_arg(args, unsigned int), 1, print_l);
 }
 
 int	ft_printf(const char *s, ...)
 {
-	int		print_l;
+	int		*print_l;
 	va_list	args;
+	int		print_c;
 
-	print_l = 0;
-	va_start(args, str);
+	print_c = 0;
+	print_l = &print_c;
+	va_start(args, s);
 	while (*s)
 	{
 		if (*s == 37)
-			print_l += ft_callarg(args, ++s);
+		{
+			s++;
+			ft_callarg(args, *s, print_l);
+		}
 		else
-			print_l += ft_putchar_fd(*s, 1);
+			ft_putchar_fd(*s, 1, print_l);
 		s++;
 	}
+	printf("%d\n", print_c);
 	va_end(args);
-	return (print_l);
+	return (print_c);
 }
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,29 +6,25 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 21:05:58 by htrindad          #+#    #+#             */
-/*   Updated: 2024/05/09 19:39:29 by htrindad         ###   ########.fr       */
+/*   Updated: 2024/05/10 18:48:07 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_printf.h"
 
-static int	g_size = 0;
-
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd, int *len)
 {
 	if (n == -2147483648)
-		g_size = ft_putstr_fd("-2147483648", fd);
+		ft_putstr_fd("-2147483648", fd, len);
 	else
 	{
-		g_size++;
 		if (n < 0)
 		{
-			ft_putchar_fd(45, fd);
+			ft_putchar_fd(45, fd, len);
 			n = -n;
 		}
 		if (n > 9)
-			ft_putnbr_fd(n / 10, fd);
-		ft_putchar_fd(n % 10 + 48, fd);
+			ft_putnbr_fd(n / 10, fd, len);
+		ft_putchar_fd(n % 10 + 48, fd, len);
 	}
-	return (g_size);
 }
